c71bf80ca8d18316fc9c8aa533d7dbc1812ce417
Dhiraj Kumar <er.dhiraj.86@gmail.com>
2025-09-08 01:43:16 +0530

    Update provider.tf

--- Environment/Dev/provider.tf
+++ Environment/Dev/provider.tf
@@ -6,6 +6,10 @@ terraform {
     }
   }
   backend "azurerm" {
+    use_oidc = true
+    client-id: da20dc07-bace-4293-ab7b-ff449a8911aa
+    tenant-id: defb5fbe-74e0-453c-9d33-594936bf34c5
+    subscription-id: 81cc1915-8d88-419e-8fa3-0178811761bd
     resource_group_name  = "dhiraj-rg"
     storage_account_name = "dhirajstorage11"
     container_name       = "dhiraj-blob-container"
@@ -16,6 +20,5 @@ terraform {
 
 provider "azurerm" {
   features {}
- 
   subscription_id = "81cc1915-8d88-419e-8fa3-0178811761bd"
 }
